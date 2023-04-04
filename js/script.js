561430156136e43983e02fe8aacedf02c08015d7
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-04 11:42:02 +0300

    Include code for Values and Variables

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ let js = "amazing";
 if ((js = "amazing")) {
   console.log("JavaScript is fun!");
 }
+
+// Values and Variables
+console.log("Obua"); // Obua here is a value
+
+let firstName = "Denis"; // firstName is a variable
+console.log(firstName);
+
+// There are variable naming conventions
