a1b952e7076efe51edf64c8d83d4022a36e3e031
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-04 14:36:20 +0300

    Add code demonstrating the use of template literals (aka. template strings)

---
+++ js/templateLiterals.js
@@ -0,0 +1,15 @@
+const firstName = "Denis";
+const job = "Developer";
+const currentYear = 2023;
+const birthYear = 1993;
+
+const msg = `I'm ${firstName}, a ${currentYear - birthYear} year old ${job}.`;
+
+console.log(msg);
+console.log(`Just a regular string is also acceptable.`);
+
+// Creating multiline strings
+console.log(`Strings with multiple lines.
+Template literals are amazing.
+It makes it possible.
+`);
